d2363f49c34be8ee704400e9d7bcad98ad36c895
AlperenGurdal <alpeni4848@gmail.com>
2026-06-04 19:47:57 -0400

    Complete Expense tracker with menu, view, and total

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 import json
 
-File_name = "expenses.json"
+FILE_NAME = "expenses.json"
 
 def load_expenses():
-    with open( File_name, "r") as file:
+    with open( FILE_NAME, "r") as file:
         return json.load(file)
 
 def save_expenses(expenses):
-    with open(File_name, "w") as file:
+    with open(FILE_NAME, "w") as file:
         json.dump(expenses, file, indent =4) 
 
 
 def add_expenses(expenses):
     description = input("description: ")
-    amount = float(input("amount: "))
+    try:
+        amount = float(input("amount: "))
+    except ValueError:
+        print("Invalid amount. Please enter a number.")
+        return
 
     expenses.append({"description": description,
         "amount": amount})
@@ -21,10 +25,44 @@ def add_expenses(expenses):
     save_expenses(expenses)
     print("Expense Added!")
 
+def view_expenses(expenses):
+    if not expenses:
+        print("no expenses yet")
+        return
+    
+    print("\n === Expenses ===")
+    for i, expense in enumerate(expenses, start=1):
+        print(f"{i}. {expense['description']} - ${expense['amount']:.2f}")
 
+def view_total(expenses):
+    if not expenses:
+        print("no expenses yet")
+        return
+    
+    total = sum(expense["amount"] for expense in expenses)
+    print(f"Total spent: ${total:.2f}")
 
 def main():
     expenses = load_expenses()
-    add_expenses(expenses)
+    while True:
+        print("\n===Expense Tracker===")
+        print("1. Add Expense")
+        print("2. View Expense")
+        print("3. View Total")
+        print("4. Exit")
+
+        choice = input("choose an option: ")
+
+        if choice == "1":
+            add_expenses(expenses)
+        elif choice == "2":
+            view_expenses(expenses)
+        elif choice == "3":
+            view_total(expenses)
+        elif choice == "4":
+            print("Goodbye")
+            break
+        else:
+            print("invalid choice, try again")
 
 main()
